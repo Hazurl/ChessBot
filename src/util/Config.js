d82d6d6521f36b889b3a0d1abb55e5e669b7b7a2
Hazurl <ricoujules69@gmail.com>
2019-04-03 17:25:02 +0200

    add config file

---
+++ src/util/Config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    dev_mode_enable: process.env.CHESS_BOT_DEV_MODE && true,
+    console_color_enable: process.env.CHESS_BOT_DEV_MODE && true,
+    
+    major_version: '0', 
+    minor_version: '3',
+}
